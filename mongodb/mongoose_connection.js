22ba16c685ce05fe43c539cb3bb4de2fd41ed067
Owain <owaine1@live.com>
2017-11-28 09:58:53 +0800

    No sooner had Edger Allen Poe Finished his old Raven, then he started his Old Crow.

---
+++ mongodb/mongoose_connection.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+var url = 'mongodb://localhost:27500/employees';
+mongoose.connect(url, {useMongoClient: true});
+
+var contact_structure = {
+    email: String,
+    cell: String,
+    city: String
+}
+
+var contact_schema = new mongoose.Schema(contact_structure);
+
+var employee_structure = {
+    name: String,
+    gender: String,
+    contact: contact_structure
+}
+var employee_schema = new mongoose.Schema(employee_structure);
+
+var EMPLOYEECLASS = mongoose.model('employees',employee_schema);
+
+module.exports = EMPLOYEECLASS
--- routes/main_routes.js
+++ routes/main_routes.js
@@ -3,7 +3,35 @@ module.exports = router;
 
 router.get('/', do_homepage);
 
-function do_homepage(req, res){
-console.log('do_homepage');
-res.render('index');
+function do_homepage(req, res) {
+    console.log('do_homepage');
+    res.render('index');
+}
+// api
+
+router.get('/api/v7/read', do_read);
+router.get('/api/v7/read:_id', do_single_read);
+router.post('/api/v7/read', do_create);
+router.put('/api/v7/read', do_update);
+router.delete('/api/v7/read', do_delete);
+
+function do_read(req, res) {
+    console.log('reading all records');
+
+}
+function do_single_read(req, res) {
+    console.log('reading single record');
+
+}
+function do_create(req, res) {
+    console.log('updating employee');
+    console.log(req.body)
+}
+function do_update(req, res) {
+    console.log('reading single record');
+    console.log(req.body)
+}
+function do_delete(req, res) {
+    console.log('reading single record');
+    console.log(req.params)
 }
